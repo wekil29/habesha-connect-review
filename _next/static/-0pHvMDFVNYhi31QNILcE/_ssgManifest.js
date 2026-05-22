0b696fdd8ab1d83126a5e90db174cbc014287269
Wakil Muzeyen <wakilmuzeyen@Mac.lan>
2026-05-21 20:08:43 -0500

    Deploy phase 4 marketplace review

--- _next/static/-0pHvMDFVNYhi31QNILcE/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fbusinesses\u002F[slug]","\u002Fevents\u002F[slug]","\u002Fposts\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
---
+++ _next/static/cxMe8pDenogiG0etSmnmk/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fbusinesses\u002F[slug]","\u002Fevents\u002F[slug]","\u002Fmarketplace\u002F[slug]","\u002Fposts\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
